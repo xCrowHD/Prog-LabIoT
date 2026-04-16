54cfb465bdeead504367c5fec2433cadbe7ae7b3
Mattia Foggetti <102517665+xCrowHD@users.noreply.github.com>
2026-04-16 23:14:25 +0200

    shift and Add More Logic in MQTTHandler

--- SerraESP/MqttHandler.h
+++ SerraESP/MqttHandler.h
@@ -3,10 +3,20 @@
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-
+#include <ArduinoJson.h>
 
 #define TOPIC_THRESHOLD "lab_iot/mafogani/threshold"
 #define TOPIC_START_STOP "lab_iot/mafogani/start-stop"
+
+struct Thresholds {
+  char platName[32];
+  float tempMin;
+  float tempMax;
+  float humMin;
+  float humMax;
+  int luxMin;
+  int luxMax;
+};
 
 
 class MqttHandler {
@@ -14,6 +24,8 @@ private:
   PubSubClient _client;
   const char* _broker;
   int _port;
+  Thresholds _plantThresholds;
+  bool _isStartMode;
 
 public:
   // Costruttore
@@ -24,6 +36,13 @@ public:
   void handle();
   void reconnect();
   bool connected();
+  void processMessage(char* topic, byte* payload, unsigned int length);
+  Thresholds getThresholds();
+  bool isRunning();
+
+private:
+  void handleThresholds(byte* payload, unsigned int length);
+  void handleStartStop(byte* payload, unsigned int length);
 };
 
 #endif
