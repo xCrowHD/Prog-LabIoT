bd9f5c617dfe4f194862f30c0620798b624c3157
Mattia Foggetti <foggetti2307@gmail.com>
2026-05-11 15:49:42 +0200

    Update esp status is now pooled

--- WebApp/static/main.js
+++ WebApp/static/main.js
@@ -400,31 +400,17 @@ async function getNextNodeMcu() {
   const response = await fetch("/api/plants/nextnode");
   const data = await response.json();
   //console.log(data);
-  if (data.status == "OFFLINE") {
-    document
-      .getElementById("esp-status")
-      .classList.replace("text-primary", "text-red");
-    document
-      .getElementById("eps-status-icon")
-      .classList.replace("bg-primary", "bg-red");
-  } else {
-    document
-      .getElementById("esp-status")
-      .classList.replace("text-red", "text-primary");
-    document
-      .getElementById("eps-status-icon")
-      .classList.replace("bg-red", "bg-primary");
-  }
-
-  document.getElementById("node-id").innerText =
-    `Monitoring Node: MCU-${data.id}`;
-  document.getElementById("esp-status").innerText = `System ${data.status}`;
+  _changeMcuStatus(data);
 }
 
 async function getCurrentNodeMcu() {
   const response = await fetch("/api/plants/currentnode");
   const data = await response.json();
   //console.log(data);
+  _changeMcuStatus(data);
+}
+
+async function _changeMcuStatus(data) {
   if (data.status == "OFFLINE") {
     document
       .getElementById("esp-status")
@@ -473,6 +459,10 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("Boot avviato...");
   loadAtStart();
   getCurrentNodeMcu();
+  const intervalId = setInterval(async () => {
+    getCurrentNodeMcu();
+  }, 5000);
+
   document.getElementById("plant-loop").addEventListener("click", loopPlants);
 
   document
