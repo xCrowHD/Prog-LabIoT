96ce1c6442ce4e4d30afa73e87729e6bce008fa9
Mattia Foggetti <foggetti2307@gmail.com>
2026-06-08 17:38:32 +0200

    Update app.py

--- WebApp/app.py
+++ WebApp/app.py
@@ -53,4 +53,4 @@ async def home():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="127.0.0.1", port=8001, reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
